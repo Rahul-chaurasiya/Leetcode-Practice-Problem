417b1758c86aae7553944c9a68edc3f8e8230419
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-08-24 23:56:20 +0530

    Time: 2 ms (78.43%), Space: 41.5 MB (50.11%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int num) {
+        
+        int rem;
+        long reversed=0;
+		while(num!=0)
+		{
+			rem = num%10;
+			num = num/10;
+            reversed = reversed * 10 + rem;
+			// if(rem != 0)
+			// System.out.print(rem);
+		}
+        if( reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE)
+        return 0;
+        
+        else return (int)reversed;
+        }
+        
+    
+}
