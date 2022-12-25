fbf06d50b84bb11bf0881022ba165929b05bd5b5
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2022-12-25 06:58:36 +0530

    Create Majority Element.java

---
+++ Majority Element/Majority Element.java
@@ -0,0 +1,32 @@
+
+
+class Solution {
+    public int majorityElement(int[] arr) {
+        int res = 0, count = 1;
+        int n = arr.length;
+
+    	for(int i = 1; i < n; i++)
+    	{
+    		if(arr[res] == arr[i])
+    			count++;
+    		else 
+    			count --;
+
+    		if(count == 0)
+    		{
+    			res = i; count = 1;
+    		}
+    	}
+
+    	count = 0;
+
+    	for(int i = 0; i < n; i++)
+    		if(arr[res] == arr[i])
+    			count++;
+
+    	if(count <= n /2)
+    		res = -1;
+
+    	return arr[res];
+    }
+}
