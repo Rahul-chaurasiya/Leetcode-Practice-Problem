f4e11273cc0581f17aa4f251aa6a168d2fcf23b3
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-01-03 20:28:53 +0530

    Update Power of Three.java

--- Power of Three/Power of Three.java
+++ Power of Three/Power of Three.java
@@ -4,3 +4,18 @@ class Solution {
         return (p - (int)p == 0);
     }
 }
+
+
+=======================================================
+    
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1)
+            return true;
+        
+        if(n<=0 || n%3!=0) 
+            return false;
+        
+        return isPowerOfThree(n/3);
+    }
+}
