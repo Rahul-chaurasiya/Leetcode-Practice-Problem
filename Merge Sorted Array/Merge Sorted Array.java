ece3e561f64ff9f328a5305fa9f640ceacf6de27
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-01-09 20:38:58 +0530

    Update Merge Sorted Array.java

--- Merge Sorted Array/Merge Sorted Array.java
+++ Merge Sorted Array/Merge Sorted Array.java
@@ -14,3 +14,15 @@ class Solution {
     }
 }
 
+
+==========================================================================
+    
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+        for(int i=m;i<nums1.length;i++,j++){
+            nums1[i] = nums2[j];
+        }
+        Arrays.sort(nums1);
+    }
+}
