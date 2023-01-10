b6fb8cfbd712018cb138fcb3e3373ca1c18e4bba
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-01-11 00:02:02 +0530

    Create Happy Number.java

---
+++ Happy Number/Happy Number.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public boolean isHappy(int n) {
+            int sum=0;
+            while(n>0){
+                int a = n%10;
+                sum += a*a;
+                n /=10;
+            }
+            if(sum == 1){
+                return true;
+            }
+            else if(sum==4){
+                return false;
+            }
+        
+        return isHappy(sum);
+    }
+}
