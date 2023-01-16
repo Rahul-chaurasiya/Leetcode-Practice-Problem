5d8c1cde54e8355f26f7b206e7af5a68aed49d5d
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-01-16 19:42:16 +0530

    Update Rotate Array Right.java

--- Rotate Array RIght/Rotate Array Right.java
+++ Rotate Array RIght/Rotate Array Right.java
@@ -21,3 +21,33 @@
             System.arraycopy(result, 0, nums, 0, nums.length);
         }
     }   
+
+
+
+========================================================================
+    
+// efficient solution
+    
+  class Solution {
+        public void rotate(int[] nums, int d) {
+            if (d > nums.length) {
+                d = d % nums.length;
+            }
+            int n = nums.length-d;
+            rotate(nums, 0, n - 1);
+            rotate(nums, n, nums.length - 1);
+            rotate(nums, 0, nums.length - 1);
+            
+        }
+
+
+    static void rotate(int arr[], int low, int high) {
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}   
