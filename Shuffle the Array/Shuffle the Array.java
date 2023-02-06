95bf4b19e1b2666171736eecb07c9616403c9fa0
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-02-06 21:36:31 +0530

    Update Shuffle the Array.java

--- Shuffle the Array/Shuffle the Array.java
+++ Shuffle the Array/Shuffle the Array.java
@@ -1,5 +1,22 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
+        List<Integer> l = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            l.add(nums[i]);
+            l.add(nums[i+n]);
+        }
+        int[] array = new int[l.size()];
+        for(int i = 0; i < l.size(); i++) array[i] = l.get(i);
+        return array;
+    }
+}
+
+
+=================================================================
+    
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
         int ans[] = new int[2*n];
         int k=0;
         for(int i=0;i<n;i++){
