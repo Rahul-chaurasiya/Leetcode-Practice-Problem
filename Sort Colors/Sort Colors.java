58090e110114da8175d97a1b4617ea2ac74b5013
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-02-22 12:15:41 +0530

    Update Sort Colors.java

--- Sort Colors/Sort Colors.java
+++ Sort Colors/Sort Colors.java
@@ -18,3 +18,22 @@ class Solution {
         }
     }
 }
+
+
+
+=======================================================================
+    
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] colors = new int[3];
+        for (int i = 0; i < nums.length; i++)
+            colors[nums[i]] = colors[nums[i]] + 1;
+
+        int index = 0;
+        for (int i = 0; i < colors.length; i++) {
+            for (int j = 0; j < colors[i]; j++)
+                nums[index++] = i;
+        }
+    }
+}
