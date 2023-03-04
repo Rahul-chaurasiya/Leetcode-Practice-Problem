9c7d66f761219c4e397d99fe835196289e340152
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-03-04 13:53:49 +0530

    Create Single Number II.java

---
+++ Single Number II/Single Number II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
+        int count=1;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i - 1])
+                count++;
+            if (nums[i] != nums[i - 1] ){
+                if(count != 3) return nums[i-1];
+                count=1;
+            }
+        }
+        return 0;
+    }
+}
