caf5fdf92381aba19840e7ad0a302dcce3128e99
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-03-04 14:12:24 +0530

    Create First Unique Character in a String.java

---
+++ First Unique Character in a String/First Unique Character in a String.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int firstUniqChar(String str) {
+        int[] fI = new int[26];
+        Arrays.fill(fI, -1);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (fI[str.charAt(i)-'a'] == -1)
+                fI[str.charAt(i)-'a'] = i;
+            else
+                fI[str.charAt(i)-'a'] = -2;
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < fI.length; i++) {
+            if (fI[i] >= 0)
+                res = Math.min(res, fI[i]);
+        }
+        return (res == Integer.MAX_VALUE) ? -1 : res;
+    }
+}
+
+
+==========================================================================
+  
+  
+class Solution {
+    public int firstUniqChar(String str) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (count[str.charAt(i)-'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
