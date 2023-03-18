aa08d380430d8b7dcb5605b3cdb2f0e7ddef1e5c
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-03-18 16:14:47 +0530

    Update Determine Color of a Chessboard Square.java

--- Determine Color of a Chessboard Square/Determine Color of a Chessboard Square.java
+++ Determine Color of a Chessboard Square/Determine Color of a Chessboard Square.java
@@ -14,3 +14,14 @@ class Solution {
         return ans%2!=0;
     }
 }
+
+
+==============================================================================
+    
+    
+    
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        return ((coordinates.charAt(0)-'a')+(coordinates.charAt(1)-'0'))%2==0;
+    }
+}
