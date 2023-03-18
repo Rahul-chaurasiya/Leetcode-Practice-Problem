00f5ad7b4ef0dcd50fc0cc620a56d766932e0319
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-03-18 16:09:28 +0530

    Create Determine Color of a Chessboard Square.java

---
+++ Determine Color of a Chessboard Square/Determine Color of a Chessboard Square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean squareIsWhite(String s) {
+        int ans=0;
+        if(s.charAt(0)=='a') ans=1;
+        if(s.charAt(0)=='b') ans=2;
+        if(s.charAt(0)=='c') ans=3;
+        if(s.charAt(0)=='d') ans=4;
+        if(s.charAt(0)=='e') ans=5;
+        if(s.charAt(0)=='f') ans=6;
+        if(s.charAt(0)=='g') ans=7;
+        if(s.charAt(0)=='h') ans=8;
+
+        ans = ans + Integer.parseInt(s, 1, s.length(), 10);
+        return ans%2!=0;
+    }
+}
