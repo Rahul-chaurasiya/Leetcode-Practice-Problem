85f1c3b0b79278cee3cf3e1afe654a398e9c42d7
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-05-30 07:54:53 +0530

    Create Design HashSet.java

---
+++ Design HashSet/Design HashSet.java
@@ -0,0 +1,18 @@
+class MyHashSet {
+    HashSet<Integer>hs;
+    public MyHashSet() {
+        hs = new HashSet<>();
+    }
+    
+    public void add(int key) {
+        hs.add(key);
+    }
+    
+    public void remove(int key) {
+        hs.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return hs.contains(key);
+    }
+}
