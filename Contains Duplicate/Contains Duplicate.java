d922459c23a7af188f4938f3922446258bc1dd1c
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-08-12 00:27:52 +0530

    Update Contains Duplicate.java

--- Contains Duplicate/Contains Duplicate.java
+++ Contains Duplicate/Contains Duplicate.java
@@ -14,3 +14,19 @@ class Solution {
         return false;
     }
 }
+
+
+==========================================================================================
+
+
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : arr) {
+            if (hs.contains(num))
+                return true;
+            hs.add(num);
+        }
+        return false;
+    }
+}
