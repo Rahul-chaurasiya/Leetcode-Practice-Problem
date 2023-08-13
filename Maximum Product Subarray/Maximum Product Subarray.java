ceb6c9d11c8a36d1e9dc5a1abc599efb5c458b44
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-08-13 23:57:07 +0530

    Create Maximum Product Subarray.java

---
+++ Maximum Product Subarray/Maximum Product Subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans = 1;
+        int max = nums[0];
+
+        for(int i=0;i<nums.length;i++){
+            ans = ans*nums[i];
+            max = Math.max(ans,max);
+            if(ans == 0) ans = 1;
+        }
+
+        ans = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            ans = ans*nums[i];
+            max = Math.max(ans,max);
+            if(ans == 0) ans = 1;
+        }
+        
+        return max;
+    }
+}
