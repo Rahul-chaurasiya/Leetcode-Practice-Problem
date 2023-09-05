d77cdda373cc6de9b25a097944be3928b3a9ed9e
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2023-09-05 18:00:20 +0530

    Create Factorial Trailing Zeroes.java

---
+++ Factorial Trailing Zeroes/Factorial Trailing Zeroes.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        for (int i = 5; i <= n; i=i*5) {
+            ans = ans+n/i;
+        }
+        return ans;
+    }
+}
