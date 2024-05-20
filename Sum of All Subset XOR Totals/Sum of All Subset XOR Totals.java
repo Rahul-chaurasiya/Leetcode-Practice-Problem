0c0f3bc3392f97525541f6eebd24b21861c5f6c9
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2024-05-21 00:19:22 +0530

    Create Sum of All Subset XOR Totals.java

---
+++ Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int sumTotal = 0;
+
+        for (int num : nums) {
+            sumTotal |= num;
+        }
+        return sumTotal << (nums.length - 1);
+    }
+}
