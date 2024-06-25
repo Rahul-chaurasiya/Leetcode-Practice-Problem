daf40067271647329043f4c7b3f3ffebd35b62ce
Rahul chaurasiya <77222540+Rahul-chaurasiya@users.noreply.github.com>
2024-06-25 23:26:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            int n = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            int k = Integer.parseInt(S[2]);
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String S1[] = read.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = Integer.parseInt(S1[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans[][] = ob.rotateMatrix(k, mat);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for java
+
+class Solution {
+    int[][] rotateMatrix(int k, int mat[][]) {
+        int c=mat[0].length;
+       int r=mat.length;
+       k%=c;
+       int a[][]=new int[r][c];
+       for(int i=0;i<r;i++)
+       {
+           for(int j=0;j<c;j++)
+           {
+               int nc=(j-k+c)%c;
+               a[i][nc]=mat[i][j];
+           }
+       }
+       return a;
+    }
+}
